49e8c63798841591db3211a231c1f3d7b108eb13
Gary Yu <gairy.yu@gmail.com>
2018-04-19 22:17:08 +0800

    add test_fig4()

--- blockdag/src/lib.rs
+++ blockdag/src/lib.rs
@@ -14,9 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the rust-dag library. If not, see <http://www.gnu.org/licenses/>.
 
-extern crate core;
-
 pub mod blockdag;
+
 
 #[cfg(test)]
 mod tests {
@@ -46,6 +45,43 @@ mod tests {
         dag_add_block("K", &vec!["B","H","I"], &mut dag);
         dag_add_block("L", &vec!["D","I"], &mut dag);
         dag_add_block("M", &vec!["F","K"], &mut dag);
+
+        dag_print(&dag);
+
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_fig4() {
+
+        let mut dag: HashMap<String, Arc<RwLock<Block>>> = HashMap::new();
+
+        dag_add_block("Genesis", &Vec::new(), &mut dag);
+
+        dag_add_block("B", &vec!["Genesis"], &mut dag);
+        dag_add_block("C", &vec!["Genesis"], &mut dag);
+        dag_add_block("D", &vec!["Genesis"], &mut dag);
+        dag_add_block("E", &vec!["Genesis"], &mut dag);
+
+        dag_add_block("F", &vec!["B","C"], &mut dag);
+        dag_add_block("H", &vec!["E"], &mut dag);
+        dag_add_block("I", &vec!["C","D"], &mut dag);
+
+        dag_add_block("J", &vec!["F","D"], &mut dag);
+        dag_add_block("K", &vec!["J","I","E"], &mut dag);
+        dag_add_block("L", &vec!["F"], &mut dag);
+        dag_add_block("N", &vec!["D","H"], &mut dag);
+
+        dag_add_block("M", &vec!["L","K"], &mut dag);
+        dag_add_block("O", &vec!["K"], &mut dag);
+        dag_add_block("P", &vec!["K"], &mut dag);
+        dag_add_block("Q", &vec!["N"], &mut dag);
+
+        dag_add_block("R", &vec!["O","P","N"], &mut dag);
+
+        dag_add_block("S", &vec!["Q"], &mut dag);
+        dag_add_block("T", &vec!["S"], &mut dag);
+        dag_add_block("U", &vec!["T"], &mut dag);
 
         dag_print(&dag);
 
--- godag/src/main.rs
+++ godag/src/main.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the rust-dag library. If not, see <http://www.gnu.org/licenses/>.
 
+
+
 fn main() {
     println!("Hello, world!");
 }
