d7a1d09bab0d765c0fd226b6e869c19f34db8aab
Gary Yu <gairy.yu@gmail.com>
2018-04-23 09:46:51 +0800

    still having deadlock

--- blockdag/src/blockdag/calcblue.rs
+++ blockdag/src/blockdag/calcblue.rs
@@ -46,7 +46,7 @@ pub fn calc_blue(block_name: &str, node: &mut Node, k: i32){
     }
 
     // step 2
-    let mut tip_max_name = String::from(block_name);
+    let mut tip_max_name = String::from(block_name.clone());
     let mut max_past_blue: u64 = block_r.size_of_past_blue;
     let tips = &node.tips;
     if tips.len() == 0 {
@@ -55,49 +55,81 @@ pub fn calc_blue(block_name: &str, node: &mut Node, k: i32){
     }
 
     for (key, value) in tips {
-        if key.eq(&tip_max_name) {
-            continue;
-        }
-        println!("calc_blue(): block {}. come to tip {}, block_name={}", block_name, key, block_name);
+//        if key.eq(&tip_max_name) {
+//            continue;
+//        }
+//        println!("calc_blue(): block {}. come to tip {}", block_name, key);
 
         let tip = &value.read().unwrap();
+        if &tip.name == &tip_max_name {
+            continue;
+        }
+        println!("calc_blue(): block {}. come to tip {}", block_name, key);
+
         if tip.size_of_past_blue > max_past_blue {
             max_past_blue = tip.size_of_past_blue;
             tip_max_name = tip.name.clone();
         }
     }
 
+    println!("calc_blue(): block {}.tip_max_name={},max_past_blue={}", block_name, tip_max_name, max_past_blue);
+
     // step 3
     if &tip_max_name == block_name {
 
+        println!("calc_blue(): block {}. new block is the max past blue", block_name);
+
         // step 4
         drop(block_r);
-        let mut block_w = dag.get(block_name).unwrap().write().unwrap();
-        block_w.is_blue = true;
-        drop(block_w);
 
-        let block_r = dag.get(block_name).unwrap().read().unwrap();
+        {
+            let (blues, blue_anticone) = tips_anticone_blue(block_name, tips, k);
+            if blues >= 0 && blues <= k {
+                // step 4.1
+                let mut block_w = dag.get(block_name).unwrap().write().unwrap();
+                block_w.is_blue = true;
+                block_w.size_of_anticone_blue = blues;
+                drop(block_w);
+                println!("calc_blue(): block {}. add {} to the blue. size_of_anticone_blue={}", block_name, block_name, blues);
+
+                // step 4.2
+                check_blue(&blue_anticone, k);
+            } else {
+                println!("calc_blue(): block {}. error! should be blue, but anticone blues={}", block_name, blues);
+            }
+            drop(blue_anticone);
+        }
 
         // step 5
+        let block_r = dag.get(block_name).unwrap().read().unwrap();
         for (name, value) in &block_r.prev {
 
-            let mut pred = value.write().unwrap();
+            let pred = value.read().unwrap();
             if pred.is_blue {
                 continue;
             }
+            drop(pred);
 
             // step 6
-            let (blues,blue_anticone) = tips_anticone_blue(name, &block_r.prev, k);
-            if blues>=0 && blues<=k {
-                // step 7
-                //let pred = &Arc::clone(value).write().unwrap();
-                pred.is_blue = true;
+            {
+                let (blues, blue_anticone) = tips_anticone_blue(name, &block_r.prev, k);
+                if blues >= 0 && blues <= k {
+                    // step 7
+                    let mut pred = value.write().unwrap();
+                    pred.is_blue = true;
+                    pred.size_of_anticone_blue = blues;
+                    println!("calc_blue(): block {}. add {} to the blue. size_of_anticone_blue={}", block_name, pred.name, blues);
+                    drop(pred);
 
-                // step 8
-                check_blue(&blue_anticone, k);
+                    // step 8
+                    check_blue(&blue_anticone, k);
+                }
+                drop(blue_anticone);
             }
         }
     }else{
+
+        println!("calc_blue(): block {}. new block is not the max past blue", block_name);
 
         // step 11
         let (blues,blue_anticone) = tips_anticone_blue(block_name, tips, k);
@@ -110,6 +142,7 @@ pub fn calc_blue(block_name: &str, node: &mut Node, k: i32){
             //let pred = &Arc::clone(value).write().unwrap();
             block_w.is_blue = true;
             block_w.size_of_anticone_blue = blues;
+            println!("calc_blue(): block {}. add {} to the blue. size_of_anticone_blue={}", block_name, block_w.name, blues);
             drop(block_w);
 
             // step 13
@@ -125,11 +158,15 @@ fn check_blue(blue_anticone: &HashMap<String, Arc<RwLock<Block>>>, k: i32) {
     for (key, value) in blue_anticone {
 
         let mut block = value.write().unwrap();
-        if block.size_of_anticone_blue == k {
+        if block.size_of_anticone_blue >= k {
             block.is_blue = false;
             block.size_of_anticone_blue = -1;
+            println!("check_blue(): remove {} from blue", block.name);
 
-            minus_successors_anticone_blue(&block);
+            dec_successors_anticone_blue(&block);
+        }else if block.is_blue{
+            block.size_of_anticone_blue += 1;
+            println!("check_blue(): {} size_of_anticone_blue increase to {}", block.name, block.size_of_anticone_blue);
         }
     }
 }
@@ -138,7 +175,7 @@ fn check_blue(blue_anticone: &HashMap<String, Arc<RwLock<Block>>>, k: i32) {
 ///
 /// todo: this iteration could be terrible in performance!
 ///
-fn minus_successors_anticone_blue(block: &Block){
+fn dec_successors_anticone_blue(block: &Block){
 
     for (key, value) in &block.next {
 
@@ -146,6 +183,6 @@ fn minus_successors_anticone_blue(block: &Block){
         if next.is_blue {
             next.size_of_past_blue -= 1;
         }
-        minus_successors_anticone_blue(&next);
+        dec_successors_anticone_blue(&next);
     }
 }
